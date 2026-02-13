74e5b7b2f5bcdc595f5fc4477810f1b4e9256909
Tausif Rahman <tausif.rahman@datadoghq.com>
2026-02-13 12:38:56 -0500

    changes

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ setup_web_ui() {
 
     # Install yarn switch
     curl -sS https://repo.yarnpkg.com/install | bash
-    source ~/.zshrc
+    export PATH="$HOME/.yarn/switch/bin:$PATH"
 
     yarn
     yarn install
